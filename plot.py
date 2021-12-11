e3fac747e2ba322e6af1a61e466b9613da95e831
Omer Tuchfeld <otuchfel@redhat.com>
2021-12-11 17:34:19 +0100

    Improved plotting code

--- plot.py
+++ plot.py
@@ -10,24 +10,35 @@ script_dir = Path(__file__).parent.absolute()
 
 
 def parse_line(line):
+    # Example line:
+    # 833MiB 0:00:01 [ 833MiB/s] [<=>                                               ]
+    # Extracts ("833", "MiB")
     pat = re.compile(r"[0-9.a-zA-Z]+ [0-9:]+ \[([0-9.]+)([a-zA-Z]+)/s\].*")
     results = pat.findall(line)
-    if results:
-        return results[0]
 
-    return (None, None)
+    if results:
+        value, unit = results[0]
+        return float(value) / (1 if unit == "GiB" else 1024)
+
+    return 0.0
 
 
 def main():
     with open(script_dir / "results.json", "r") as f:
         results = json.load(f)
 
-    parsed_results = {k: [parse_line(line) for line in v.splitlines()] for k, v in results.items()}
+    def remove_none(x):
+        return x[1]
+
+    parsed_results = {
+        k: [
+            parse_line(line) for line in v.splitlines()
+        ] for k, v in results.items()
+    }
 
     for submission, values in parsed_results.items():
-        values_float = [float(value) / (1 if unit == "GiB" else 1024) for value, unit in values if value]
-        mean = np.mean(values_float)
-        p = plt.plot(values_float, ".", label=submission)
+        p = plt.plot(values, ".", label=submission)
+        mean = np.mean(values)
         plt.axhline(mean, color=p[0].get_color())
 
     plt.legend()
