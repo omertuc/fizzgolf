0c3e56bdd0bfd51d97db826007ceccb264a75483
Omer Tuchfeld <otuchfel@redhat.com>
2021-12-12 14:53:31 +0100

    Make it possible to update just a single submission
    Useful when someone changes their implementation

--- score.py
+++ score.py
@@ -9,6 +9,7 @@ import time
 import json
 import signal
 import os
+import sys
 from collections import defaultdict
 
 
@@ -46,15 +47,35 @@ def test_submission(submission: Path):
 
 
 def main():
+    if len(sys.argv) == 3:
+        # Run just the language/submission combo specified in the command line
+        force = (sys.argv[1], sys.argv[2])
+    else:
+        # Run all languages/submissions
+        force = None
+
+    results_file = script_dir / "results.json"
+
     results = defaultdict(dict)
+    if force is not None:
+        # Try to load all results from previous results file
+        if results_file.exists():
+            with open(results_file, "r") as f:
+                results = json.load(f)
+
     for lang in (script_dir / "submissions").glob("*"):
         for submission in lang.glob("*"):
-            print(submission.iterdir())
             if submission.is_dir() and not any(child.name == "TBD" for child in submission.iterdir()):
-                print("Testing {lang.name} {submission.name}")
+                if force is not None:
+                    force_language, force_submission = force
+                    if lang.name != force_language or submission.name != force_submission:
+                        print(f"Skipping {lang.name} {submission.name}")
+                        continue
+
+                print(f"Testing {lang.name} {submission.name}")
                 results[lang.name][submission.name] = test_submission(submission)
 
-    with open("results.json", "w") as f:
+    with open(results_file, "w") as f:
         json.dump(results, f)
 
 
