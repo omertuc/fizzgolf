8b6c460ee2d63e0eaaa90e0fadbc0dda376cf212
Omer Tuchfeld <otuchfel@redhat.com>
2021-12-23 18:55:30 +0100

    Simplify tester

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -24,41 +24,19 @@ const BUFFER_SIZE: usize = (1024 * 1024) as usize;
 
 fn seek_to_fizz_start<T: Read>(reader: &mut T) -> Result<(), std::io::Error> {
     let mut buffer = [0; 1];
-
     let mut state = 0;
 
     loop {
-        let n = reader.read(&mut buffer).unwrap();
-
-        if n == 0 {
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Fizz not found",
-            ));
-        }
-
+        reader.read_exact(&mut buffer)?;
         state = match state {
-            0 => {
-                if buffer[0] == b'\n' {
-                    1
-                } else {
-                    0
-                }
+            0 if buffer[0] == b'\n' => 1,
+            0 => 0,
+            1 if buffer[0] == b'1' => 2,
+            1 => 0,
+            2 if buffer[0] == b'\n' => {
+                return Ok(());
             }
-            1 => {
-                if buffer[0] == b'1' {
-                    2
-                } else {
-                    0
-                }
-            }
-            2 => {
-                if buffer[0] == b'\n' {
-                    return Ok(());
-                } else {
-                    0
-                }
-            }
+            2 => 0,
             _ => unreachable!(),
         }
     }
@@ -83,11 +61,11 @@ fn main() {
     let mut last_progress = 0 as usize;
     let mut last_update = Instant::now();
 
+    static mut BUF1: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+    static mut BUF2: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
+
     loop {
         unsafe {
-            static mut BUF1: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-            static mut BUF2: [u8; BUFFER_SIZE] = [0; BUFFER_SIZE];
-
             stdout_first.read_exact(&mut BUF1).unwrap();
             stdout_second.read_exact(&mut BUF2).unwrap();
 
@@ -108,11 +86,9 @@ fn main() {
         }
 
         if Instant::now() - last_update > Duration::from_secs(1) {
-            println!(
-                "Verified {:.2} GiB at {:.2} GiB/s",
-                (progress as f64) / GIB,
-                (progress - last_progress) as f64 / GIB
-            );
+            let progress_gib = (progress as f64) / GIB;
+            let rate_gib = (progress - last_progress) as f64 / GIB;
+            println!("Verified {:.2} GiB at {:.2} GiB/s", progress_gib, rate_gib);
             last_update = Instant::now();
             last_progress = progress;
         }
