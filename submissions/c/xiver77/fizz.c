55a1fd44f2eff4689b46ded4a03b2691c6a0bdfd
xiver77 <xiver77@gmail.com>
2021-12-24 12:44:02 +0900

    more than 2x faster

--- submissions/c/xiver77/fizz.c
+++ submissions/c/xiver77/fizz.c
@@ -1,9 +1,12 @@
+#define _GNU_SOURCE
+#include <stdbool.h>
 #include <inttypes.h>
-#include <limits.h>
 #include <stdalign.h>
-#include <unistd.h>
+#include <sys/mman.h>
+#include <fcntl.h>
 #include <tmmintrin.h>
 
+#define BUF_SIZE 0x100000
 #define Z 0xf6f6f6f6f6f6f6f6u
 static const union {uint64_t _[2]; __m128i m;} S[] = {
     {{0x0706050403020100, 0x0f0e0d0c0b0a0908}},
@@ -44,106 +47,120 @@ static int writeDigits(char *b, __m128i d, int i) {
 }
 
 static int writeFizz(char *b) {
-    b[0] = 'F';
-    b[1] = 'i';
-    b[2] = 'z';
-    b[3] = 'z';
-    b[4] = '\n';
-    return 5;
-}
-
-static int writeBuzz(char *b) {
-    b[0] = 'B';
-    b[1] = 'u';
-    b[2] = 'z';
-    b[3] = 'z';
-    b[4] = '\n';
+    *(uint64_t *)b = 0x0a7a7a6946;
     return 5;
 }
 
 static int writeFizzBuzz(char *b) {
-    b[0] = 'F';
-    b[1] = 'i';
-    b[2] = 'z';
-    b[3] = 'z';
-    b[4] = 'B';
-    b[5] = 'u';
-    b[6] = 'z';
-    b[7] = 'z';
-    b[8] = '\n';
+    *(uint64_t *)b = 0x7a7a75427a7a6946;
+    b[8] = 0x0a;
     return 9;
+}
+
+static int writeFizzAndBuzz(char *b) {
+    *(uint64_t *)b = 0x7a75420a7a7a6946;
+    ((uint16_t *)b)[4] = 0x0a7a;
+    return 10;
+}
+
+static int writeBuzzAndFizz(char *b) {
+    *(uint64_t *)b = 0x7a69460a7a7a7542;
+    ((uint16_t *)b)[4] = 0x0a7a;
+    return 10;
 }
 
 static __m128i fat(__m128i d) {
     return _mm_or_si128(d, _mm_and_si128(_mm_set1_epi64x(Z),
-           _mm_cmpeq_epi8(d, _mm_setzero_si128())));
+                        _mm_cmpeq_epi8(d, _mm_setzero_si128())));
+}
+
+__attribute__((always_inline)) inline
+static void out(char *b, int n) {
+    struct iovec iov = {b, n};
+    do {
+        ssize_t n = vmsplice(1, &iov, 1, 0);
+        iov.iov_base = (char *)iov.iov_base + n;
+        iov.iov_len -= n;
+    } while (iov.iov_len);
 }
 
 #define I d = inc(d)
+#define IC d = inc_carry(d)
+#define FAT d = fat(d)
 #define D I; p += writeDigits(p, d, i)
 #define F I; p += writeFizz(p)
-#define B_ p += writeBuzz(p)
-#define U_ p += writeFizzBuzz(p)
-#define B I; B_
-#define U I; U_
+#define U I; p += writeFizzBuzz(p)
+#define FNB I; I; p += writeFizzAndBuzz(p);
+#define BNF I; I; p += writeBuzzAndFizz(p);
+#define FST_ D; D; F; D; BNF;  D; D; I; p += writeFizzAndBuzz(p);
+#define SND_ D; F; D; D; U; D; D; F; D; p += writeBuzzAndFizz(p);
+#define TRD_ I; D; D; FNB;  D; F; D; D; p += writeFizzBuzz(p)
+#define OUT do {\
+    if (p - b + (17 * 16 + 5 * 12 + 9 * 2) > BUF_SIZE) {\
+        out(b, p - b);\
+        p = b;\
+    }\
+} while (false)
+#define FST FST_; I; FAT
+#define SND SND_; I; FAT
+#define TRD TRD_; I; FAT; OUT
+#define FSTC FST_; IC; FAT
+#define SNDC SND_; IC; FAT
+#define TRDC TRD_; IC; FAT; OUT
+#define GROW do {\
+    if (j == k) {\
+        k *= 10;\
+        --i;\
+    }\
+} while (false)
 
 int main() {
+    fcntl(1, F_SETPIPE_SZ, BUF_SIZE);
     __m128i d = _mm_set1_epi64x(Z);
     int i = sizeof(d) - 1;
-    alignas(0x1000) char b[0x10000];
+    alignas(0x1000) static char b[BUF_SIZE];
     char *p = b;
     long long j = 10, k = 10;
-    for (; j < 100000000; j += 30) {
-        D; D; F; D; B; F; D; D; F; B;
-        if (j == k) {
-            k *= 10;
-            d = _mm_or_si128(d,
-                _mm_set_epi64x(0, Z >> (i - sizeof(uint64_t)) * CHAR_BIT));
-            --i;
-        } else {
-            d = fat(d);
-        }
-        D; F; D; D; U; D; D; F; D; B;
-        d = fat(d);
-        F; D; D; F; B; D; F; D; D; U;
-        d = fat(d);
-        if (p - b + (9 * 16 + 5 * 12 + 9 * 2) > sizeof(b)) {
-            write(1, b, p - b);
-            p = b;
-        }
+    for (; j < 100000000 - 30; j += 30) {
+        FST;
+        GROW;
+        SND;
+        TRD;
     }
-    for (; j < 10000000000000000; j += 30) {
-        D; D; F; D; B; F; D; D; F; B_;
-        if (j == k) {
-            d = inc_carry(d);
-            k *= 10;
-            d = _mm_or_si128(d, _mm_set_epi64x(Z >> (i - 1) * CHAR_BIT, Z));
-            --i;
-        } else {
-            if (!(j % 100000000)) {
-                d = inc_carry(d);
-            } else {
-                d = inc(d);
+    int l = 0;
+    for (; j < 10000000000000000; j += 100000000 / 30 * 30) {
+        switch (l) {
+            case 0: {
+                l = 2;
+                FST;
+                SND;
+                TRD;
+                j += 30;
+                FSTC;
+                GROW;
+                SND;
+                TRD;
+                break;
+            } case 1: {
+                --l;
+                FST;
+                SND;
+                TRDC;
+                break;
+            } case 2: {
+                --l;
+                FST;
+                SNDC;
+                TRD;
+                break;
+            } default: {
+                __builtin_unreachable();
             }
-            d = fat(d);
         }
-        D; F; D; D; U; D; D; F; D; B_;
-        if (!((j + 10) % 100000000)) {
-            d = inc_carry(d);
-        } else {
-            d = inc(d);
-        }
-        d = fat(d);
-        F; D; D; F; B; D; F; D; D; U_;
-        if (!((j + 20) % 100000000)) {
-            d = inc_carry(d);
-        } else {
-            d = inc(d);
-        }
-        d = fat(d);
-        if (p - b + (17 * 16 + 5 * 12 + 9 * 2) > sizeof(b)) {
-            write(1, b, p - b);
-            p = b;
+        for (int k = 0; k < 100000000 / 30 - 1; ++k) {
+            FST;
+            SND;
+            TRD;
         }
     }
     return 0;
