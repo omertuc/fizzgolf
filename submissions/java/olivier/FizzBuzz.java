cdeb759ddd0c0320e7d58276f1981a07623177c9
Omer Tuchfeld <omer@tuchfeld.dev>
2022-06-07 22:53:33 +0200

    Olivier new & arrmansa

--- submissions/java/olivier/FizzBuzz.java
+++ submissions/java/olivier/FizzBuzz.java
@@ -1,47 +1,52 @@
 import java.io.FileDescriptor;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.stream.IntStream;
 
 import static java.nio.charset.StandardCharsets.US_ASCII;
 
 public final class FizzBuzz {
 
+  private static final int BUFFER_SIZE = 1 << 16;
   private static final long STARTING_NUMBER = 10L;
-  private static final long INCREMENT = 5_400L;
   private static final long SIZE_FOR_30 = 2 * (17 * 8 + 47);
+  private static final long FULL_INCREMENT = BUFFER_SIZE / SIZE_FOR_30 * 30;
+  private static final long LIGHT_INCREMENT = FULL_INCREMENT - 30;
   private static final int[] BASE_IDX = {-1, 6, 8, 19, 21, 28, 40, 42, 54, 61, 63, 74, 76, 83, 95, 97};
+  private static final int[][] IDX = new int[19][16];
 
-  public static void main(String[] args) {
-    new FizzBuzz().run();
+  static {
+    IDX[1] = BASE_IDX;
+    for (var i = 2; i < IDX.length; i++) {
+      for (var j = 0; j < 16; ) {
+        IDX[i][j] = IDX[i - 1][j] + ++j;
+      }
+    }
   }
 
-  private void run() {
 
+  public static void main(String[] args) {
     var threads = Runtime.getRuntime().availableProcessors();
     var queue = new LinkedBlockingQueue<Future<ByteBuffer>>(threads);
     var executor = Executors.newFixedThreadPool(threads);
-    try (var channel = new FileOutputStream(FileDescriptor.out).getChannel()) {
+    try (var outputStream = new FileOutputStream(FileDescriptor.out);
+         var channel = outputStream.getChannel()) {
 
       var counter = STARTING_NUMBER;
-      System.err.println(INCREMENT * SIZE_FOR_30 / 30L);
-      for (var buffer : IntStream.range(0, threads).mapToObj(x -> ByteBuffer.allocateDirect((int) (INCREMENT * SIZE_FOR_30 / 30L))).toList()) {
+      for (var i = 0; i < threads; i++) {
+        var buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
         queue.offer(executor.submit(new Task(counter, buffer)));
-        counter += INCREMENT;
+        counter += FULL_INCREMENT;
       }
       channel.write(ByteBuffer.wrap("1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nBuzz\nFizz\n".getBytes(US_ASCII)));
       while (true) {
         var buffer = queue.poll().get();
-        buffer.flip();
         channel.write(buffer);
-        buffer.clear();
         queue.offer(executor.submit(new Task(counter, buffer)));
-        counter += INCREMENT;
+        counter += FULL_INCREMENT;
       }
     } catch (Exception e) {
       e.printStackTrace(System.err);
@@ -54,40 +59,58 @@ public final class FizzBuzz {
 
     @Override
     public ByteBuffer call() {
+      buffer.clear();
       var n = startingNumber;
-      var idx = Arrays.copyOf(BASE_IDX, BASE_IDX.length);
       var nextPowerOf10 = 10L;
-      for (; nextPowerOf10 < n; nextPowerOf10 *= 10L) {
-        for (var i = 0; i < idx.length; ) {
-          idx[i] += ++i;
-        }
+      var digitCount = 1;
+      for (; nextPowerOf10 <= n; nextPowerOf10 *= 10L) {
+        digitCount++;
       }
-      byte[] template = null;
+      var idx = IDX[digitCount];
+      var t = n / 10L;
+      var s = Long.toString(t);
+      var template = (s + "1\nFizz\n" +
+        s + "3\n" +
+        s + "4\nFizzBuzz\n" +
+        s + "6\n" +
+        s + "7\nFizz\n" +
+        s + "9\nBuzz\nFizz\n" +
+        (s = Long.toString(++t)) + "2\n" +
+        s + "3\nFizz\nBuzz\n" +
+        s + "6\nFizz\n" +
+        s + "8\n" +
+        s + "9\nFizzBuzz\n" +
+        (s = Long.toString(++t)) + "1\n" +
+        s + "2\nFizz\n" +
+        s + "4\nBuzz\nFizz\n" +
+        s + "7\n" +
+        s + "8\nFizz\nBuzz\n").getBytes(US_ASCII);
+      n += 30;
+      buffer.put(template);
 
-      for (var limit = n + INCREMENT; n < limit; n += 30) {
-        if (template == null | n == nextPowerOf10) {
-          if (n == nextPowerOf10) {
-            nextPowerOf10 *= 10;
-            for (var i = 0; i < idx.length; ) {
-              idx[i] += ++i;
-            }
-          }
-          template = ((n + 1) + "\nFizz\n" +
-              (n + 3) + "\n" +
-              (n + 4) + "\nFizzBuzz\n" +
-              (n + 6) + "\n" +
-              (n + 7) + "\nFizz\n" +
-              (n + 9) + "\nBuzz\nFizz\n" +
-              (n + 12) + "\n" +
-              (n + 13) + "\nFizz\nBuzz\n" +
-              (n + 16) + "\nFizz\n" +
-              (n + 18) + "\n" +
-              (n + 19) + "\nFizzBuzz\n" +
-              (n + 21) + "\n" +
-              (n + 22) + "\nFizz\n" +
-              (n + 24) + "\nBuzz\nFizz\n" +
-              (n + 27) + "\n" +
-              (n + 28) + "\nFizz\nBuzz\n").getBytes(US_ASCII);
+      for (var limit = n + LIGHT_INCREMENT; n < limit; n += 30L) {
+        if (n == nextPowerOf10) {
+          nextPowerOf10 *= 10;
+          digitCount++;
+          idx = IDX[digitCount];
+          t = n / 10L;
+          s = Long.toString(t);
+          template = (s + "1\nFizz\n" +
+            s + "3\n" +
+            s + "4\nFizzBuzz\n" +
+            s + "6\n" +
+            s + "7\nFizz\n" +
+            s + "9\nBuzz\nFizz\n" +
+            (s = Long.toString(++t)) + "2\n" +
+            s + "3\nFizz\nBuzz\n" +
+            s + "6\nFizz\n" +
+            s + "8\n" +
+            s + "9\nFizzBuzz\n" +
+            (s = Long.toString(++t)) + "1\n" +
+            s + "2\nFizz\n" +
+            s + "4\nBuzz\nFizz\n" +
+            s + "7\n" +
+            s + "8\nFizz\nBuzz\n").getBytes(US_ASCII);
         } else {
           for (var i = 0; i < idx.length; ) {
             var pos = idx[i++];
@@ -100,8 +123,13 @@ public final class FizzBuzz {
         }
         buffer.put(template);
       }
-      return buffer;
+      return buffer.flip();
     }
   }
 
 }
+
+/*
+ * Speed references:
+ *   - 270 GiB / min ± 3.95 / min on Macbook Pro 2021
+ */
