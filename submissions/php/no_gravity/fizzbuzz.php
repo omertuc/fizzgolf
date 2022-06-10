ff320321c567686d0c1c7c4f32b1eba37c795a12
Omer Tuchfeld <omer@tuchfeld.dev>
2022-06-10 10:40:56 +0200

    PHP casing

--- submissions/php/no_gravity/fizzbuzz.php
+++ submissions/php/no_gravity/fizzbuzz.php
@@ -5,10 +5,10 @@ ob_start();
 
 for ($i = 0; $i < PHP_INT_MAX; $i++) {
         $out = $i;
-        if ($i % 3 === 0) $out = 'fizz';
+        if ($i % 3 === 0) $out = 'Fizz';
         if ($i % 5 === 0) {
-                $out = 'buzz';
-                if ($i % 3 === 0) $out = 'fizzbuzz';
+                $out = 'Buzz';
+                if ($i % 3 === 0) $out = 'FizzBuzz';
         }
         echo "$out\n";
         if ($i % 8192 === 0) ob_flush();
