7180a88e95786ff5768c058ddb5b4b264cec90ac
Omer Tuchfeld <omer@tuchfeld.dev>
2022-06-12 16:12:45 +0200

    Phan submission new but still broken

--- submissions/rust/PhanThanhDuy/src/main.rs
+++ submissions/rust/PhanThanhDuy/src/main.rs
@@ -5,13 +5,13 @@ use libc::exit;
 use itoap;
 
 #[inline(always)]
-fn u32_to_bytes(n: u32, v: &mut Vec<u8>) {
+fn u64_to_bytes(n: u64, v: &mut Vec<u8>) {
     itoap::write_to_vec(v, n);
     v.push(0x0a);
 }
 
 fn main() {
-    let mut i: u32 = 1;
+    let mut i: u64 = 0;
 
     let Fizz = "Fizz\n".as_bytes();
     let Buzz = "Buzz\n".as_bytes();
@@ -19,65 +19,51 @@ fn main() {
 
     let mut buffering: Vec<u8> = Vec::with_capacity(1024*1024*2);
     loop {
-        for _ in 0..32 {
-            // total_bytes += nlength(i) as u64;
-            u32_to_bytes(i, &mut buffering);
+        for _ in 0..512 {
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
         
-            // total_bytes += nlength(i+1) as u64;
-            u32_to_bytes(i+1, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+2
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Fizz);
 
-            // i+3
-            // total_bytes += nlength(i+3) as u64;
-            u32_to_bytes(i+3, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+4
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Buzz);
 
-            // i+5
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Fizz);
 
-            // i+6
-            // total_bytes += nlength(i+6) as u64;
-            u32_to_bytes(i+6, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+7
-            // total_bytes += nlength(i+7) as u64;
-            u32_to_bytes(i+7, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+8
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Fizz);
 
-            // i+9
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Buzz);
 
-            // i+10
-            // total_bytes += nlength(i+10) as u64;
-            u32_to_bytes(i+10, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+11
-            // total_bytes += 5;
+            i += 1;
             buffering.extend_from_slice(&Fizz);
 
-            // i+12
-            // total_bytes += nlength(i+12) as u64;
-            u32_to_bytes(i+12, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+13
-            // total_bytes += nlength(i+13) as u64;
-            u32_to_bytes(i+13, &mut buffering);
+            i += 1;
+            u64_to_bytes(i, &mut buffering);
 
-            // i+14
-            // total_bytes += 9;
+            i += 1;
             buffering.extend_from_slice(&FizzBuzz);            
-            i += 15;
         }
         write(1, buffering.as_slice());
         buffering.clear();
