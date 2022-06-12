f1ff6fad6ec79c3292b53c3987e54d8dba24370f
Omer Tuchfeld <omer@tuchfeld.dev>
2022-06-12 16:12:08 +0200

    Better PHP

--- submissions/php/no_gravity/fizzbuzz.php
+++ submissions/php/no_gravity/fizzbuzz.php
@@ -1,15 +1,20 @@
 #!/bin/php
 <?php
 
-ob_start();
+ob_start(null, 32000);
 
-for ($i = 0; $i < PHP_INT_MAX; $i++) {
-        $out = $i;
-        if ($i % 3 === 0) $out = 'Fizz';
-        if ($i % 5 === 0) {
-                $out = 'Buzz';
-                if ($i % 3 === 0) $out = 'FizzBuzz';
-        }
-        echo "$out\n";
-        if ($i % 8192 === 0) ob_flush();
+$a = [0, 1, 'Fizz', 3, 'Buzz', 'Fizz',
+      6, 7, 'Fizz', 'Buzz', 10, 'Fizz',
+      12, 13, 'FizzBuzz', ''];
+
+for ($i = 1; $i < PHP_INT_MAX-15; $i+=15) {
+    $a[0] = $i;
+    $a[1] = $i+1;
+    $a[3] = $i+3;
+    $a[6] = $i+6;
+    $a[7] = $i+7;
+    $a[10] = $i+10;
+    $a[12] = $i+12;
+    $a[13] = $i+13;
+    echo join("\n", $a);
 }
